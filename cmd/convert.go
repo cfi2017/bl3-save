103b12d466decd5e5790d852fe30ecf45f60c3b5
Carlo Field <carlo@swiss.dev>
2020-04-23 03:54:56 +0200

    add items command, update convert command for mass input

--- cmd/convert.go
+++ cmd/convert.go
@@ -1,55 +1,141 @@
 package cmd
 
 import (
+	"bufio"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
 	"strings"
 
+	"github.com/cfi2017/bl3-save-core/pkg/assets"
 	"github.com/cfi2017/bl3-save-core/pkg/item"
 	"github.com/spf13/cobra"
+)
+
+var (
+	literals []string
+	files    []string
 )
 
 // deserializeCmd represents the deserialize command
 var ConvertCmd = &cobra.Command{
 	Use:   "convert",
 	Short: "Convert an item from gibbed to digital_marine or vice versa",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		args[0] = strings.TrimPrefix(args[0], "bl3(")
-		args[0] = strings.TrimSuffix(args[0], ")")
-		bs, err := base64.StdEncoding.DecodeString(args[0])
-		if err != nil {
-			panic(err)
+		queue := make(chan string)
+
+		go func() {
+			for literal := range queue {
+				c, err := convert(literal)
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+				fmt.Println(c)
+			}
+		}()
+
+		for _, literal := range literals {
+			queue <- literal
 		}
-		var dmi item.DigitalMarineItem
-		err = json.Unmarshal(bs, &dmi)
-		if err != nil {
-			// try deserializing item
-			i, err := item.Deserialize(bs)
-			if err != nil {
-				cmd.PrintErr("couldn't deserialize item")
+
+		for _, file := range files {
+			var reader io.Reader
+			var err error
+			if file != "-" {
+				reader, err = os.Open(file)
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+			} else {
+				reader = os.Stdin
+			}
+			scanner := bufio.NewScanner(reader)
+			for scanner.Scan() {
+				queue <- scanner.Text()
+			}
+			if err := scanner.Err(); err != nil {
+				cmd.PrintErr(err)
 				return
 			}
-			// convert to dm item
-			bs, err = json.Marshal(item.GibbedToDm(i))
-			if err != nil {
-				cmd.PrintErr("couldn't convert item to dm format")
-				return
+			if f, ok := reader.(*os.File); ok {
+				err = f.Close()
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
 			}
-			cmd.Print(base64.StdEncoding.EncodeToString(bs))
-			return
 		}
-		i := item.DmToGibbed(dmi)
-		bs, err = item.Serialize(i, 0) // encrypt with 0 seed
-		if err != nil {
-			cmd.Print("error decoding item. invalid serial?")
-		}
-		cmd.Printf("bl3(%s)", base64.StdEncoding.EncodeToString(bs))
+		close(queue)
 	},
+}
+
+type Anointments struct {
+	CopyType       string
+	ComponentNames []string
+	Components     []int
+}
+
+func convert(arg string) (string, error) {
+	anoints := make([]string, 0)
+	if parts := strings.Split(arg, " "); len(parts) > 1 {
+		var anointments Anointments
+		bs, err := base64.StdEncoding.DecodeString(parts[1])
+		if err != nil {
+			return "", err
+		}
+		err = json.Unmarshal(bs, &anointments)
+		if anointments.CopyType != "anointment" {
+			return "", errors.New("not a valid anointment code")
+		}
+		for _, i := range anointments.Components {
+			anoints = append(anoints, item.DmKeyToInvKey(anointments.ComponentNames[i],
+				assets.GetDB().GetData("InventoryGenericPartData").Assets))
+		}
+		arg = parts[0]
+	}
+	arg = strings.TrimPrefix(arg, "bl3(")
+	arg = strings.TrimSuffix(arg, ")")
+	bs, err := base64.StdEncoding.DecodeString(arg)
+	if err != nil {
+		panic(err)
+	}
+	var dmi item.DigitalMarineItem
+	err = json.Unmarshal(bs, &dmi)
+	if err != nil {
+		// try deserializing item
+		i, err := item.Deserialize(bs)
+		if err != nil {
+			return "", errors.New("couldn't deserialize item")
+		}
+		// convert to dm item
+		bs, err = json.Marshal(item.GibbedToDm(i))
+		if err != nil {
+			return "", errors.New("couldn't convert item to dm format")
+		}
+		return base64.StdEncoding.EncodeToString(bs), nil
+	}
+	i := item.DmToGibbed(dmi)
+	// we don't check for existing anoints at the moment,
+	// nor anoint count (todo: add sanity checks)
+	i.Generics = append(i.Generics, anoints...)
+	bs, err = item.Serialize(i, 0) // encrypt with 0 seed
+	if err != nil {
+		return "", errors.New("error decoding item. invalid serial")
+	}
+	return fmt.Sprintf("bl3(%s)", base64.StdEncoding.EncodeToString(bs)), nil
 }
 
 func init() {
 	rootCmd.AddCommand(ConvertCmd)
+
+	ConvertCmd.PersistentFlags().StringSliceVar(&literals, "from-literal", []string{}, "literal code inputs")
+	ConvertCmd.PersistentFlags().StringSliceVar(&files, "from-file", []string{}, "input files")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
---
+++ cmd/items.go
@@ -0,0 +1,162 @@
+package cmd
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/cfi2017/bl3-save-core/pkg/assets"
+	"github.com/cfi2017/bl3-save-core/pkg/character"
+	"github.com/cfi2017/bl3-save-core/pkg/item"
+	"github.com/cfi2017/bl3-save-core/pkg/pb"
+	"github.com/spf13/cobra"
+)
+
+var (
+	items []string
+)
+
+var ItemsCommand = &cobra.Command{
+	Use:  "items",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// first, grab the character
+		f, err := os.Open(args[0])
+		if err != nil {
+			cmd.PrintErrf("couldn't open character: %v\n", err)
+		}
+		s, c := character.Deserialize(f)
+		err = f.Close()
+		if err != nil {
+			cmd.PrintErr(err)
+			return
+		}
+
+		for _, file := range files {
+			var reader io.Reader
+			var err error
+			if file != "-" {
+				reader, err = os.Open(file)
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+			} else {
+				reader = os.Stdin
+			}
+			scanner := bufio.NewScanner(reader)
+			for scanner.Scan() {
+				items = append(items, scanner.Text())
+			}
+			if err := scanner.Err(); err != nil {
+				cmd.PrintErr(err)
+				return
+			}
+			if f, ok := reader.(*os.File); ok {
+				err = f.Close()
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+			}
+		}
+
+		for i := range items {
+			var anoints = make([]string, 0)
+			if parts := strings.Split(items[i], " "); len(parts) > 1 {
+				var anointments Anointments
+				bs, err := base64.StdEncoding.DecodeString(parts[1])
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+				err = json.Unmarshal(bs, &anointments)
+				if anointments.CopyType != "anointment" {
+					cmd.PrintErrln("not a valid anointment code")
+					return
+				}
+				for _, i := range anointments.Components {
+					anoints = append(anoints, item.DmKeyToInvKey(anointments.ComponentNames[i],
+						assets.GetDB().GetData("InventoryGenericPartData").Assets))
+				}
+				items[i] = parts[0]
+			}
+			if strings.HasPrefix(items[i], "bl3(") {
+				// assume bl3 format, verify and add
+			} else {
+				// try to convert base64
+				bs, err := base64.StdEncoding.DecodeString(items[i])
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+				var dmi item.DigitalMarineItem
+				err = json.Unmarshal(bs, &dmi)
+				if err == nil {
+					gi := item.DmToGibbed(dmi)
+					bs, err = item.Serialize(gi, 0) // encrypt with 0 seed
+					if err != nil {
+						cmd.PrintErr(err)
+						return
+					}
+					items[i] = hex.EncodeToString(bs)
+				}
+				// else assume bl3 format, do nothing for now
+			}
+
+			items[i] = strings.TrimPrefix(items[i], "bl3(")
+			items[i] = strings.TrimSuffix(items[i], ")")
+
+			bs, err := base64.StdEncoding.DecodeString(items[i])
+			if err != nil {
+				cmd.PrintErr(err)
+				return
+			}
+			// we don't actually care about the item,
+			// just that it deserializes correctly
+			bsc := make([]byte, len(bs))
+			copy(bsc, bs)
+			current, err := item.Deserialize(bsc)
+			if err != nil {
+				cmd.PrintErr(err)
+				return
+			}
+			current.Generics = append(current.Generics, anoints...)
+			bs, err = item.Serialize(current, 0)
+			if err != nil {
+				cmd.PrintErr(err)
+				return
+			}
+			c.InventoryItems = append(c.InventoryItems, &pb.OakInventoryItemSaveGameData{
+				ItemSerialNumber:    bs,
+				PickupOrderIndex:    200, // set static, idk what this does
+				Flags:               1,   // flag 1 should be "new"
+				WeaponSkinPath:      "",  // no skin applied
+				DevelopmentSaveData: nil,
+			})
+		}
+
+		// first, grab the character
+		f, err = os.Create(args[0])
+		if err != nil {
+			cmd.PrintErrf("couldn't create character: %v\n", err)
+		}
+		character.Serialize(f, s, c)
+		err = f.Close()
+		if err != nil {
+			cmd.PrintErr(err)
+			return
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(ItemsCommand)
+	ItemsCommand.PersistentFlags().StringSliceVar(&files, "from-file", []string{}, "import items from file (- for stdin)")
+	ItemsCommand.PersistentFlags().StringSliceVar(&items, "from-literal", []string{}, "import item")
+}
